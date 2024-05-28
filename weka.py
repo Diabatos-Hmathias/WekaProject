f212b04d8e4e8a153dccf72e7554a806343d4596
Χρήστος <74558267+Kremmudis@users.noreply.github.com>
2024-05-28 22:48:53 +0300

    Update weka.py

--- weka.py
+++ weka.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template, flash, redirect, url_for
+import os
 
 app = Flask(__name__)
 app.secret_key = 'supersecretkey'
@@ -74,4 +75,5 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
